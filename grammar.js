e619244731e92e80ec7962c499f948ca46fa90a9
munkybooty <shrunkymunky@icloud.com>
2022-10-19 01:30:44 -0400

    update grammar.js to include rules, comments, and rulesets

--- grammar.js
+++ grammar.js
@@ -2,7 +2,37 @@ module.exports = grammar({
   name: 'twitchspawnlanguage',
 
   rules: {
+
+    extras: $ => [
+      $.comment,
+    ],
+
+    externals: $ => [
+      $._newline,
+      $._indent,
+      $.comment,
+
+      // Allow the external scanner to check for the validity of closing brackets
+      // so that it can avoid returning dedent tokens between brackets.
+      ']',
+      '}',
+    ],
+
     // TODO: add the actual grammar rules
-    source_file: $ => 'hello'
+    ruleset: $ => repeat(choice(
+      $.rule,
+      $.meta_action_rule,
+      $.advanced_action_rule,
+    )),
+
+    rule: $ => repeat(choice(
+      $.action,
+      $.comparator
+      $.event,
+    )),
+
+    comment: $ => token(seq('#', /.*/)),
+
   }
+
 });
